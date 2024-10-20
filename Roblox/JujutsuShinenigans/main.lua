609166053f28852b563c436057b4dbe938cfaf89
skibidiMusic <skibidimusic723@gmail.com>
2024-10-20 14:34:34 +0300

    Update main.lua

--- Roblox/JujutsuShinenigans/main.lua
+++ Roblox/JujutsuShinenigans/main.lua
@@ -87,6 +87,7 @@ local Window = ImGui:CreateWindow({
 	Title = "JUJUT-SAKSO SHIT-A-NIGGA-NS",
 	Position = UDim2.new(0.5, 0, 0, 70), --// Roblox property 
 	Size = UDim2.new(0, 300, 0, 500),
+	AutoSize = false,
 	NoClose = true
 })
 Window:Center()
