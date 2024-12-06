d3c6fd79fc13ec1aa85d25ada5496aac524b31c3
skibidiMusic <skibidimusic723@gmail.com>
2024-12-06 17:32:14 +0300

    Update ImGui.lua

--- Roblox/Util/UiLib/ImGui.lua
+++ Roblox/Util/UiLib/ImGui.lua
@@ -2054,7 +2054,7 @@ function ImGui:CreateModal(Config)
 	return Config
 end
 
-local GuiParent = IsStudio and PlayerGui or CoreGui
+local GuiParent = IsStudio and PlayerGui or gethui() or CoreGui
 ImGui.ScreenGui = ImGui:CreateInstance("ScreenGui", GuiParent, {
 	DisplayOrder = 9999,
 	ResetOnSpawn = false
